29dc3eb1bfa3015796a2ae1efd63706653f8a66d
David Anderson <david@alpinegizmo.com>
2022-02-04 09:51:30 +0100

    Improved implementation of UsageRecordDeserializationSchema

--- src/main/java/com/ververica/flink/example/datausage/records/UsageRecordDeserializationSchema.java
+++ src/main/java/com/ververica/flink/example/datausage/records/UsageRecordDeserializationSchema.java
@@ -18,8 +18,7 @@
 
 package com.ververica.flink.example.datausage.records;
 
-import org.apache.flink.api.common.serialization.DeserializationSchema;
-import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.api.common.serialization.AbstractDeserializationSchema;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.json.JsonMapper;
@@ -27,24 +26,21 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import java.io.IOException;
 
-public class UsageRecordDeserializationSchema implements DeserializationSchema<UsageRecord> {
+public class UsageRecordDeserializationSchema
+        extends AbstractDeserializationSchema<UsageRecord> {
+
     private static final long serialVersionUID = 1L;
 
-    private static final ObjectMapper objectMapper =
-            JsonMapper.builder().build().registerModule(new JavaTimeModule());
+    private transient ObjectMapper objectMapper;
+
+    @Override
+    public void open(InitializationContext context) throws Exception {
+        super.open(context);
+        objectMapper = JsonMapper.builder().build().registerModule(new JavaTimeModule());
+    }
 
     @Override
     public UsageRecord deserialize(byte[] message) throws IOException {
         return objectMapper.readValue(message, UsageRecord.class);
-    }
-
-    @Override
-    public boolean isEndOfStream(UsageRecord nextElement) {
-        return false;
-    }
-
-    @Override
-    public TypeInformation<UsageRecord> getProducedType() {
-        return TypeInformation.of(UsageRecord.class);
     }
 }
