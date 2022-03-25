7e1bb570b76ef0c8c72beb43f41d498af0fb25f4
David Anderson <david@alpinegizmo.com>
2022-03-25 10:43:27 +0100

    create BenchmarkJob

--- src/main/java/com/ververica/flink/example/datausage/sources/UsageRecordGenerator.java
+++ src/main/java/com/ververica/flink/example/datausage/sources/UsageRecordGenerator.java
@@ -18,14 +18,17 @@
 
 package com.ververica.flink.example.datausage.sources;
 
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.streaming.api.functions.source.RichParallelSourceFunction;
+import org.apache.flink.types.Row;
 
 import com.ververica.flink.example.datausage.records.UsageRecord;
 
 import java.time.Instant;
 import java.util.Random;
 
-public class UsageRecordGenerator extends RichParallelSourceFunction<UsageRecord> {
+public class UsageRecordGenerator extends RichParallelSourceFunction<Row> {
 
     private static final long serialVersionUID = 1L;
 
@@ -35,24 +38,21 @@ public class UsageRecordGenerator extends RichParallelSourceFunction<UsageRecord
     public static final long DELTA_T =
             MILLISECONDS_PER_DAY / (NUMBER_OF_ACCOUNTS_PER_INSTANCE * EVENTS_PER_DAY_PER_ACCOUNT);
     public static final Instant BEGINNING = Instant.parse("2021-10-01T00:00:00.00Z");
-    public static final Instant SLOWDOWN = Instant.parse("2021-10-26T17:00:00.00Z");
 
     private volatile boolean running = true;
     private int indexOfThisSubtask;
 
     @Override
-    public void run(SourceContext<UsageRecord> ctx) throws Exception {
+    public void run(SourceContext<Row> ctx) throws Exception {
 
         this.indexOfThisSubtask = getRuntimeContext().getIndexOfThisSubtask();
         UsageRecordIterator usageRecordIterator = new UsageRecordIterator(indexOfThisSubtask);
 
         while (running) {
             UsageRecord event = usageRecordIterator.next();
-            ctx.collect(event);
-            // slow down once the timestamps catch up to the date of Flink Forward
-            if (event.ts.compareTo(SLOWDOWN) > 0) {
-                Thread.sleep(DELTA_T);
-            }
+            ctx.collect(Row.of(
+                    event.account, event.bytesUsed, event.ts
+            ));
         }
     }
 
@@ -86,5 +86,13 @@ public class UsageRecordGenerator extends RichParallelSourceFunction<UsageRecord
             return UsageRecord.accountForSubtaskAndIndex(
                     indexOfThisSubtask, random.nextInt(NUMBER_OF_ACCOUNTS_PER_INSTANCE));
         }
+    }
+
+    public static TypeInformation<Row> typeProduced() {
+        return Types.ROW_NAMED(
+                new String[] {"account", "bytesUsed", "ts"},
+                Types.STRING,
+                Types.LONG,
+                TypeInformation.of(Instant.class));
     }
 }
